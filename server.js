e58efd7bebcb20391c29fcc5c6e0323c45d14a0a
Folasayo Samuel <folasayosamuel@gmail.com>
2022-05-27 17:07:56 +0100

    update

---
+++ server.js
@@ -0,0 +1,83 @@
+// import
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import cors from "cors";
+
+// app config
+const app = express();
+const port = process.env.PORT || 5000;
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APPID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: "mt1",
+  useTLS: true,
+});
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// DB config
+const connection_url = process.env.MONGODB_ATLAS;
+
+mongoose.connect(connection_url, () => console.log(`Database Connected!`));
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB Involving Pusher Connected!");
+
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log("A change occured", change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("Error triggering Pusher");
+    }
+  });
+});
+
+// ??????
+
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+// listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
